9445ecad9d12be87f7fd5316472dcf7c78c989ce
ggiannetti <ggiannetti@citywire.co.uk>
2023-08-10 15:52:11 +0100

    adding all current files

---
+++ CelestialQuest/CelestialQuest.cs
@@ -0,0 +1,15 @@
+namespace CelestialQuest
+{
+    public partial class CelestialQuest : Form
+    {
+        public CelestialQuest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+    }
+}
